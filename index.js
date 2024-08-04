840ce8fdc52561d0c2de90ce4af80bc7ff2a4b59
Kartikeya Sharma <kartikeyasharma@Kartikeyas-MacBook-Pro.local>
2024-08-04 14:44:43 +0530

    new api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const SMTPServer = require("smtp-server").SMTPServer;
+// const SMTPServer = require("smtp-server").SMTPServer;
 const express = require('express');
 const cors = require('cors');
 
@@ -14,26 +14,26 @@ app.get('/abc', (req, res) => {
     res.send({'data' : 'json'});
   });
 
-const server = new SMTPServer({
-    allowInsecureAuth:true,
-    authOptional:true,
-    onConnect(session , cb){
-        console.log("onConnect" , session.id);
-        cb();
-    },
-    onMailFrom(address , session , cb){
-        console.log('onMailFrom' , address.address , session.id);
-        cb();
-    },
-    onRcptTo(){
-        console.log('onRcptTo' , address.address , session.id);
-    },
-    onData(stream , session , cb){
-       stream.on('data',(data) => console.log(`data ${data.toString()} `));
-       stream.end('end' , cb());
-    }
-});
-server.listen(25 , ()=> console.log("server is running on the port 25"));
+// const server = new SMTPServer({
+//     allowInsecureAuth:true,
+//     authOptional:true,
+//     onConnect(session , cb){
+//         console.log("onConnect" , session.id);
+//         cb();
+//     },
+//     onMailFrom(address , session , cb){
+//         console.log('onMailFrom' , address.address , session.id);
+//         cb();
+//     },
+//     onRcptTo(){
+//         console.log('onRcptTo' , address.address , session.id);
+//     },
+//     onData(stream , session , cb){
+//        stream.on('data',(data) => console.log(`data ${data.toString()} `));
+//        stream.end('end' , cb());
+//     }
+// });
+// server.listen(25 , ()=> console.log("server is running on the port 25"));
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
